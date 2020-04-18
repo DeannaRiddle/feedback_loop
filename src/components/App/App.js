043c8a549b19ee7fc02cba46298b1cf75dfd4c69
Deanna Riddle <lriddle.deanna@gmail.com>
2020-04-18 18:22:41 -0500

    adding components to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import FeelingPage from "../Pages/Feeling/Feeling";
+import UnderstandingPage from "../Pages/Understanding/Understanding";
+import SupportedPage from "../Pages/Supported/Supported";
+import CommentsPage from "../Pages/Comments/Comments";
+import ReviewPage from "../Pages/Review/Review";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
+          <Router>
+            <h1 className="App-title">Feedback!</h1>
+            <h4>
+              <i>Don't forget it!</i>
+            </h4>
+          </header>
+        </div>
+        <br />
+        <br />
+        <div>
+          <Route exact path="/" component={TypePage} />
+          <Route exact path="/feeling" component={FeelingPage} />
+          <Route exact path="/understanding" component={UnderstandingPage} />
+          <Route exact path="/supported" component={SupportedPage} />
+          <Route exact path="/comments" component={CommentsPage} />
+          <Route exact path="/review" component={ReviewPage} />
+        </Router>
+
+        <br />
       </div>
     );
   }
