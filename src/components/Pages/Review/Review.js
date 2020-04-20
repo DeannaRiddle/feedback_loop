9a8fcace0f0714fbec34f65035dc922dab8a0fee
Deanna Riddle <lriddle.deanna@gmail.com>
2020-04-19 20:46:31 -0500

    debugging code and testing

--- src/components/Pages/Review/Review.js
+++ src/components/Pages/Review/Review.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
-import { render } from "react-dom";
 
 class ReviewPage extends Component {
   submitFeedback = (event) => {
@@ -24,39 +23,33 @@ class ReviewPage extends Component {
   };
 
   render() {
-    let feedbackElement = (
-      <div>
-        <h2>Review Your Feedback for Today:</h2>
-      </div>
-    );
-    const reviewArray = this.props.store.summaryReducer.map(
-      (feedback, index) => {
-        return (
-          <div>
-            <h4>
-              {" "}
-              How are you feeling?:
-              {this.props.feelingReducer.feeling}
-            </h4>
-            <h4>
-              {" "}
-              How well are you understanding the content?:
-              {this.props.understandingReducer.understanding}
-            </h4>
-            <h4>
-              {" "}
-              Do you feel supported?:
-              {this.props.supportedReducer.supported}
-            </h4>
-            <h4>
-              {" "}
-              Comments:
-              {this.props.commentsReducer.comments}
-            </h4>
-          </div>
-        );
-      }
-    );
+    const reviewArray = this.props.store.summaryReducer.map((review, index) => {
+      return (
+        <div>
+          <h4>
+            {" "}
+            How are you feeling?:
+            {this.props.feelingReducer.feeling}
+          </h4>
+          <h4>
+            {" "}
+            How well are you understanding the content?:
+            {this.props.understandingReducer.understanding}
+          </h4>
+          <h4>
+            {" "}
+            Do you feel supported?:
+            {this.props.supportedReducer.supported}
+          </h4>
+          <h4>
+            {" "}
+            Comments:
+            {this.props.commentsReducer.comments}
+          </h4>
+        </div>
+      );
+    });
+    return <div> {reviewArray} </div>;
   }
 }
 
